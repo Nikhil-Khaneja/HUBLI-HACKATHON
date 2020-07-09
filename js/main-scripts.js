e7898cba024c246ac15cdabf23b5563cd890031e
Nikhil Khaneja <nikhilkhaneja7057@gmail.com>
2020-07-09 23:31:11 +0530

    New changes

---
+++ js/main-scripts.js
@@ -0,0 +1,23 @@
+// index.html main-scripts
+
+$(function(){
+    
+    $('#gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery:{ enabled:true },
+        mainClass: 'mfp-with-zoom',
+        
+        zoom:{
+            enabled: true,
+            
+            duration: 300,
+            easing: 'ease-in-out',
+            
+            opener: function(openerElement){
+                return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        }
+        
+    });
+});
--- js/scripts.js
+++ js/scripts.js
@@ -12,82 +12,95 @@ $(function(){
    $('.cover-carousel').owlCarousel(
     {
         items:1,
+        autoplay: true,
+        loop:true
     }); 
    
    $('.gallery-content').owlCarousel({
       items:1,
-      dots:false,
+      dots:false
       
       
    });
    
    $('.browser-carousel').owlCarousel({
         items:5,
-       dots:false
-       
+       dots:false      
     }); 
    
       $(".testimonials-carousel").owlCarousel({
       items:2,
-      autoplay:true,
+      autoplay:false,
       smartSpeed:700,
-      loop:true,
+      loop:false,
       autoplayHoverPause:true,
       dots:true,
    });
    
-   $('#gallery').magnificPopup({
-        delegate: 'a',
-        type: 'image',
-        gallery:{ enabled:true },
-        mainClass: 'mfp-with-zoom',
+//    $('#gallery').magnificPopup({
+//         delegate: 'a',
+//         type: 'image',
+//         gallery:{ enabled:true },
+//         mainClass: 'mfp-with-zoom',
         
-        zoom:{
-            enabled: true,
+//         zoom:{
+//             enabled: true,
             
-            duration: 300,
-            easing: 'ease-in-out',
+//             duration: 300,
+//             easing: 'ease-in-out',
             
-            opener: function(openerElement){
-                return openerElement.is('img') ? openerElement : openerElement.find('img');
-            }
-        }
+//             opener: function(openerElement){
+//                 return openerElement.is('img') ? openerElement : openerElement.find('img');
+//             }
+//         }
         
-    });
-   
-       $(window).scroll(function(){
-        if($(this).scrollTop() > 100){
+//     });
+    //    $(window).scroll(function(){
+    //     if($(this).scrollTop() > 100){
+    //         $("nav").addClass("wg-top-nav");
+    //         $(".smooth-scroll").css("color","black");
+    //         $(".navbar-brand img").attr("src","images/logo-2.png");
+    //         $(".navbar-header").css("height","100px")
+    //         $(".btn-back-to-top").fadeIn();
+            
+    //     }
+    //     else{
+    //          $("nav").removeClass("wg-top-nav");
+    //             $(".smooth-scroll").css("color","white");
+    //          $(".navbar-brand img").attr("src","images/logo-small.png");
+    //          $(".btn-back-to-top").fadeOut();
+    //     }
+    //    });
+    // $("a.smooth-scroll").click(function(event){
+    //     //Prevents to perform the default operation
+    //     event.preventDefault();
+        
+    //     var section = $(this).attr("href");
+    //     $("html, body").animate({
+    //         scrollTop: $(section).offset().top - 69,
+    //     }, 1250, "easeInOutExpo");
+    // });
+    $(window).scroll(function(){
+        //window.alert('hello');
+        if($(this).scrollTop()>100){
             $("nav").addClass("wg-top-nav");
-            $(".smooth-scroll").css("color","white");
-            $(".navbar-brand img").attr("src","images/logo-small.png");
-            $(".navbar-header").css("height","100px")
             $(".btn-back-to-top").fadeIn();
-            
         }
         else{
-             $("nav").removeClass("wg-top-nav");
-                $(".smooth-scroll").css("color","black");
-             $(".navbar-brand img").attr("src","images/logo-2.png");
-             $(".btn-back-to-top").fadeOut();
+            $("nav").removeClass("wg-top-nav");
+            $(".btn-back-to-top").fadeOut();
         }
-       });
-    $("a.smooth-scroll").click(function(event){
-        //Prevents to perform the default operation
-        event.preventDefault();
-        
-        var section = $(this).attr("href");
-        $("html, body").animate({
-            scrollTop: $(section).offset().top - 69,
-        }, 1250, "easeInOutExpo");
     });
     
-    if($(this).scrollTop() > 100){
-        $("nav").addClass("wg-top-nav");
-        $(".btn-back-to-top").fadeIn();
-    }
-    else{
-         $("nav").removeClass("wg-top-nav");
-         $(".btn-back-to-top").fadeOut();
-    }
-   
+    $("a.smooth-scroll").click(function(event){
+        //prevents to perform the default operation
+        //event.preventDefault();
+        
+        var section = $(this).attr("href");
+        $("html,body").animate({
+            scrollTop: $(section).offset().top - 69,
+        },1250, "easeInOutExpo");
+    });
+    
+
 });
